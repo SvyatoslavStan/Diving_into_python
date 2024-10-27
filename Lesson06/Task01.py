0698651e26369991c6a354b0dbfa44646f77d59d
Станислав Хилюк <geffie@yandex.ru>
2024-10-27 16:54:00 +0300

    Dummy comment, which have no sense, like all our life

---
+++ Lesson06/Task01.py
@@ -0,0 +1,11 @@
+# Задание №1
+# � Вспомните какие модули вы уже проходили на курсе.
+# � Создайте файл, в котором вы импортируете встроенные в
+# модуль функции под псевдонимами. (3-7 строк импорта).
+
+from random import randint as rnd
+from math import sqrt as st
+from sys import argv as av
+from time import time as ti
+from fractions import Fraction as Fr
+from decimal import Decimal as Dl
